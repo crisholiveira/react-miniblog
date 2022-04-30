6a587ca241e18a937c51f0dd89b1c6217d21b2fd
crisholiveira <cris.ho.soares@gmail.com>
2022-04-30 17:49:46 -0300

    hooks de autenticacao

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,8 @@
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signOut} from 'firebase/auth';
+import { useState, useEffect } from 'react';
+
+export const useAuthentication = () => {
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+}
