ab91d5997ffbd363418dd6480d785c8fafc9a156
crisholiveira <cris.ho.soares@gmail.com>
2022-05-01 13:38:23 -0300

    validacao de dados

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -13,31 +13,48 @@ const CreatePost = () => {
   const [formError, setFormError] = useState("");
   const { user } = useAuthValue()
 
-  const { insertDocument, response } = useInsertDocument("posts")
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("")
 
     //validar url da imagem
+    try {
+      new URL(image);
+
+      
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.")
+      
+    }
 
 
     // criar array de tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
 
 
     //checar todos os valores
+    if(!title || !image || !tags || !body){
+      setFormError("Por favor, preencha todos os campos")
+    }
+    if(formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createdBy: user.displayName
 
     })
 
     //redirect da home
+    navigate("/")
 
 
   }
@@ -67,6 +84,7 @@ const CreatePost = () => {
         {!response.loading && <button className='btn'>Cadastrar</button>}
         {response.loading && <button className='btn' disabled>Aguarde ...</button>}
         {response.error && <p className='error'>{response.error}</p>}
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
