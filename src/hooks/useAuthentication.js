606400ddaa866a83ccf4958163a6bbbe508613b6
crisholiveira <cris.ho.soares@gmail.com>
2022-05-01 09:54:39 -0300

    login de usuario

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -61,7 +61,36 @@ export const useAuthentication = () => {
     const logout = () => {
         checkIfIsCancelled()
         signOut(auth)
+    };
+
+    //login 
+    const login = async(data) => {
+        checkIfIsCancelled()
+        setLoading(true)
+        setError(false)
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            setLoading(false);
+
+        } catch (error) {
+            let systemErrorMessage;
+
+            if(error.message.includes("user-not-found")){
+                systemErrorMessage = "Usuário não encontrado"
+            } else if (error.message.includes("wrong-password")){
+                systemErrorMessage = "Senha incorreta"
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor, tente mais tarde"
+
+            }
+
+            setError(systemErrorMessage);
+            setLoading(false)
+            
+        }
     }
+
 
     useEffect(() => {
         return () => setCancelled(true);
@@ -73,6 +102,7 @@ export const useAuthentication = () => {
         error,
         loading,
         logout,
+        login,
     };
 
 };
