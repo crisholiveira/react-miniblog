aedd9fef06a7650c5b84976bb721d70d7b764fdc
crisholiveira <cris.ho.soares@gmail.com>
2022-05-01 09:00:10 -0300

    bloqueando paginas sem autenticacao

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
 import styles from './About.module.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className={styles.about}>
+            <h2>Sobre o Mini <span>Blog</span></h2>
+            <p>Este projeto foi desenvolvido com React no front-end e Firebase no back-end</p>
+            <Link to="/posts/create" className='btn'>Criar Post</Link>
         </div>
     )
 }
